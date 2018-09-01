84cd58c6248c209ca77d680cb0da92d92b4821b7
Moniruzzaman Monir <imonir.com@gmail.com>
2018-09-01 21:03:05 +0600

    script updated

--- scripts/bash.py
+++ scripts/bash.py
@@ -6,8 +6,5 @@ import utils
 
 utils.goToParentDir()
 
-showLog = f"CURRENT_UID={str(os.geteuid())}:{str(os.getegid())} docker-compose exec fullstack-frontend bash"
-
-#  startFrontendContainer = f"CURRENT_UID={str(os.geteuid())}:{str(os.getegid())} docker-compose -f docker-compose.yml -f docker-compose.override.yml up -d"
-#  subprocess.call(['bash', '-c', showLog])
+showLog = f"CURRENT_UID={str(os.geteuid())}:{str(os.getegid())} docker-compose exec fullstack-backend bash"
 os.system(showLog)
